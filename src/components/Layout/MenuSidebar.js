b5544bf3215345afb4814ba44f0c289533e125db
Renan Pantoja <renan.pantoja@apassos.com.br>
2020-09-08 10:13:49 -0400

    sidebar styles

--- src/components/Layout/MenuSidebar.js
+++ src/components/Layout/MenuSidebar.js
@@ -1,21 +1,38 @@
-import React, {useState} from 'react';
+import React from 'react';
 import styled from 'styled-components';
+import { GrClose } from "react-icons/gr";
 
 const SidebarMenu = styled.div`
     height: 100vh;
-    width: 50px;
+    width: 0px;
     position: fixed;
     z-index: 1;
     top: 0;
     left: 0;
     overflow-x: hidden;
     padding-top: 20px;
-    background-color: red;
+    background-color: #F5F5F6;
+    left: -100%;
     animation: slide-open 1s forwards;
+
+    @keyframes slide-open{
+        100% {left: 0}
+    }
 `;
 
+export const CloseMenu = styled(GrClose)`
+    font-size: 20px;
+    position: absolute;
+    right: 0;
+    margin-right: 10px;
+    cursor: pointer;
+`
+
 export const MenuSidebar = () => {
+
     return(
-        <SidebarMenu/>
+        <SidebarMenu>
+            <CloseMenu/>
+        </SidebarMenu>
     )
 }
